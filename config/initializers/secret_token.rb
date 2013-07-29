c7140d4d0799666ea741744a87493e7b5b541a53
chadfaling <chadfaling@gmail.com>
2013-07-29 00:17:42 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoTime2::Application.config.secret_key_base = '1c03d038bbcb5d19886360da8419d70cb6e2bdec0ec18d357d1cc99873d552858fb1b607548ebbe96f07e36f69465d933434e8d91e19371f481d1b5ee988af4d'
