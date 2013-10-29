c15b734676a16c97d2703b5b8f84d7284c10428f
chadfaling <chadfaling@gmail.com>
2013-10-29 15:52:00 -0400

    added validates to app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :phone_number, presence: true
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip_code, presence: true
 end
